dd5a0039a9992adc3ada75b4c92bf230665a4fa5
240160 <240160@jc-21.jp>
2025-06-23 14:45:07 +0900

    修正忘れ

--- Stage.h
+++ Stage.h
@@ -1,27 +1,24 @@
-#pragma once
-#include "GameObject.h"
-#include <vector>
-#include"Bullet.h"
-#include "Player.h"
+#pragma once  
+#include "GameObject.h"  
+#include <vector>  
+#include "Bullet.h"  
+#include "Player.h"  
 
-class Player;//プレイヤーの前方宣言(ポインタ)
-class Enemy;//敵の前方宣言(ポインタ)
+class Player; // プレイヤーの前方宣言(ポインタ)  
+class Enemy; // 敵の前方宣言(ポインタ)  
 
+class Stage :  
+   public GameObject  
+{  
+private:  
+   Player* player_; // プレイヤーのポインタ  
+   std::vector<Enemy*> enemy_; // 敵のポインタ  
+   int hBackground; // 背景のハンドル  
 
-class Stage :
-    public GameObject
-{
-private:
-	Player* player_;//プレイヤーのポインタ
-	std::vector <Enemy*> enemy_;//敵のポインタ
-	int hBackground;
-	Player* player_;
-	
-public:
-	Stage();
-	~Stage();
-	void Update()override;
-	void Draw() override;
-	Player* GetPlayer() { return player_; }
+public:  
+   Stage();  
+   ~Stage();  
+   void Update() override;  
+   void Draw() override;  
+   Player* GetPlayer() { return player_; }  
 };
-
